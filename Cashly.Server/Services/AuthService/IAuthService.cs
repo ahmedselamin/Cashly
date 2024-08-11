77278350e23280f2836f7163dbb9cd5a157d2aca
eddyiiz <eddyxsayf@gmail.com>
2024-08-11 06:46:05 +0200

    Register method

---
+++ Cashly.Server/Services/AuthService/IAuthService.cs
@@ -0,0 +1,10 @@
+﻿namespace Cashly.Server.Services.AuthService;
+
+public interface IAuthService
+{
+    Task<ServiceResponse<int>> Register(User user, string password);
+    Task<ServiceResponse<string>> Login(string username, string password);
+    Task<ServiceResponse<bool>> ChangePassword(int userId, string newPassword);
+    Task<ServiceResponse<bool>> DeleteUser(int userId);
+
+}
