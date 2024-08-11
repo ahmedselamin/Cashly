5715875cff8fc49cd26311a29d64e6ba22dae294
eddyiiz <eddyxsayf@gmail.com>
2024-08-11 10:09:51 +0200

    Get expense method

--- Cashly.Server/Services/ExpenseService/ExpenseService.cs
+++ Cashly.Server/Services/ExpenseService/ExpenseService.cs
@@ -1,5 +1,4 @@
-﻿
-namespace Cashly.Server.Services.ExpenseService;
+﻿namespace Cashly.Server.Services.ExpenseService;
 
 public class ExpenseService : IExpenseService
 {
@@ -9,25 +8,106 @@ public class ExpenseService : IExpenseService
     {
         _context = context;
     }
-    public Task<ServiceResponse<List<Expense>>> GetExpenses()
+    public async Task<ServiceResponse<List<Expense>>> GetExpenses(int userId)
     {
-        throw new NotImplementedException();
-    }
-    public Task<ServiceResponse<Expense>> GetExpenseById(int id)
-    {
-        throw new NotImplementedException();
-    }
-    public Task<ServiceResponse<Expense>> CreateExpense(Expense expense)
-    {
-        throw new NotImplementedException();
+        var response = new ServiceResponse<List<Expense>>();
+        try
+        {
+            response.Data = await _context.Expenses
+                .Where(e => e.UserId == userId)
+                .OrderByDescending(e => e.CreatedAt)
+                .ToListAsync();
+        }
+        catch (Exception ex)
+        {
+            response.Success = false;
+            response.Message = ex.Message;
+        }
+
+        return response;
     }
 
-    public Task<ServiceResponse<Expense>> UpdateExpense(int id, Expense newExpense)
+    public async Task<ServiceResponse<Expense>> GetExpenseById(int userId, int ExpenseId)
     {
-        throw new NotImplementedException();
+        var response = new ServiceResponse<Expense>();
+
+        try
+        {
+            var expense = await _context.Expenses
+                .FirstOrDefaultAsync(e => e.Id == ExpenseId && e.UserId == userId);
+
+            if (expense == null)
+            {
+                response.Success = false;
+                response.Message = "Not Found!";
+
+                return response;
+            }
+
+            response.Data = expense;
+        }
+        catch (Exception ex)
+        {
+
+            response.Success = false;
+            response.Message = ex.Message;
+        }
+
+        return response;
+
     }
-    public Task<ServiceResponse<bool>> DeleteExpense(int id)
+
+    public async Task<ServiceResponse<Expense>> CreateExpense(int userId, Expense expense)
     {
-        throw new NotImplementedException();
+        var response = new ServiceResponse<Expense>();
+
+        try
+        {
+
+        }
+        catch (Exception ex)
+        {
+
+            response.Success = false;
+            response.Message = ex.Message;
+        }
+
+        return response;
+    }
+
+    public async Task<ServiceResponse<Expense>> UpdateExpense(int userId, int ExpenseId, Expense newExpense)
+    {
+        var response = new ServiceResponse<Expense>();
+
+        try
+        {
+
+        }
+        catch (Exception ex)
+        {
+
+            response.Success = false;
+            response.Message = ex.Message;
+        }
+
+        return response;
+    }
+
+    public async Task<ServiceResponse<bool>> DeleteExpense(int userId, int ExpenseId)
+    {
+        var response = new ServiceResponse<bool>();
+
+        try
+        {
+
+        }
+        catch (Exception ex)
+        {
+
+            response.Success = false;
+            response.Message = ex.Message;
+        }
+
+        return response;
     }
 }
