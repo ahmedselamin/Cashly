06fcc53809ec83b0e385080f8b64114a2b5c8467
eddyiiz <eddyxsayf@gmail.com>
2024-08-11 05:55:58 +0200

    user model, expense model, and service response model

---
+++ Cashly.Server/Models/Expense.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Cashly.Server.Models;
+
+public class Expense
+{
+    public int Id { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public decimal Amount { get; set; }
+    public DateTime Date { get; set; }
+    public int UserId { get; set; }
+    [Required]
+    public User User { get; set; }
+}
+
---
+++ Cashly.Server/Models/User.cs
@@ -0,0 +1,12 @@
+﻿namespace Cashly.Server.Models;
+
+public class User
+{
+    public int Id { get; set; }
+    public string Username { get; set; } = string.Empty;
+    public byte[] PasswordHash { get; set; } = new byte[32];
+    public byte[] PasswordSalt { get; set; } = new byte[32];
+    public DateTime DateCreated { get; set; } = DateTime.Now;
+    public List<Expense> Expenses { get; set; } = new List<Expense>();
+}
+
