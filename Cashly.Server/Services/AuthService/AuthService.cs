8b6b1c396a7c73741d390d9775c8a6a0fecc8dcc
eddyiiz <eddyxsayf@gmail.com>
2024-08-11 07:54:25 +0200

    Delete user method

--- Cashly.Server/Services/AuthService/AuthService.cs
+++ Cashly.Server/Services/AuthService/AuthService.cs
@@ -119,9 +119,35 @@ public class AuthService : IAuthService
         return response;
     }
 
-    public Task<ServiceResponse<bool>> DeleteUser(int userId)
+    public async Task<ServiceResponse<bool>> DeleteUser(int userId)
     {
-        throw new NotImplementedException();
+        var response = new ServiceResponse<bool>();
+        try
+        {
+            var user = await _context.Users.FindAsync(userId);
+
+            if (user == null)
+            {
+                response.Success = false;
+                response.Message = "Not Found";
+
+                return response;
+            }
+
+            _context.Users.Remove(user);
+            await _context.SaveChangesAsync();
+
+            response.Message = "Account Deleted Successfuly.";
+            response.Data = true;
+
+        }
+        catch (Exception ex)
+        {
+            response.Success = false;
+            response.Message = ex.Message;
+        }
+
+        return response;
     }
 
     public async Task<bool> UserExists(string username)
