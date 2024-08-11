3498c07188a9259200a101b507e13941e1e3c0f9
eddyiiz <eddyxsayf@gmail.com>
2024-08-11 06:51:33 +0200

    register route

---
+++ Cashly.Server/Controllers/AuthController.cs
@@ -0,0 +1,29 @@
+﻿using Cashly.Server.Services.AuthService;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cashly.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthService _authService;
+
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+        [HttpPost]
+        public async Task<ActionResult<ServiceResponse<int>>> Register(UserRegister request)
+        {
+            var response = await _authService.Register(
+             new User
+             {
+                 Username = request.Username
+             },
+             request.Password);
+
+            return Ok(response);
+        }
+    }
+}
