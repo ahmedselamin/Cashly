110b10b526680e14530b1b3c8ff5c81a7536e2f3
eddyiiz <eddyxsayf@gmail.com>
2024-08-11 06:04:02 +0200

    DbSet & one to many relationship

--- Cashly.Server/Data/DataContext.cs
+++ Cashly.Server/Data/DataContext.cs
@@ -2,4 +2,14 @@
 
 public class DataContext(DbContextOptions<DataContext> options) : DbContext(options)
 {
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<User>()
+            .HasMany(u => u.Expenses)
+            .WithOne(e => e.User)
+            .HasForeignKey(e => e.UserId);
+    }
+
+    public DbSet<User> Users { get; set; }
+    public DbSet<Expense> Expenses { get; set; }
 }
