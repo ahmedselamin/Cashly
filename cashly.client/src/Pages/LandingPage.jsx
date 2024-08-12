665829b8dc954559042e921d1752d77d56cf63b1
eddyiiz <eddyxsayf@gmail.com>
2024-08-12 08:11:33 +0200

    Hero section animation

--- cashly.client/src/Pages/LandingPage.jsx
+++ cashly.client/src/Pages/LandingPage.jsx
@@ -13,7 +13,11 @@ const LandingPage = () => {
         <Box sx={{ flexGrow: 1 }}>
             <AppBar position="sticky" color="secondary" elevation={0} sx={{ mt: 2, py: 1, borderBottom: '2px solid #f4f6f7', }}>
                 <Toolbar>
-                    <Typography variant="h3" sx={{ flexGrow: 1, fontFamily: 'Dancing Script', fontWeight: "400" }}>
+                    <Typography variant="h3" sx={{
+                        flexGrow: 1,
+                        fontFamily: 'Dancing Script',
+                        fontWeight: "400",                        
+                    }}>
                         Cashly
                     </Typography>
                     <Button color="primary" sx={{ fontWeight: "600", ml: 4 }} onClick={handleScrollToFeatures}>
@@ -28,7 +32,17 @@ const LandingPage = () => {
                 </Toolbar>
             </AppBar>
 
-            <Container maxWidth="md" sx={{ textAlign: 'center', mt: 15 }}>
+            <Container maxWidth="md"
+                sx={{
+                    textAlign: 'center'
+                    , mt: 15,
+                    animation: 'slideIn 1s ease-out',
+                    '@keyframes slideIn': {
+                        '0%': { transform: 'translateY(20px)', opacity: 0 },
+                        '100%': { transform: 'translateY(0)', opacity: 1 },
+                    }
+                }}
+            >
                 <Typography variant="h1" gutterBottom sx={{ letterSpacing: '0.10em' }}>
                     Welcome to Cashly!
                 </Typography>
@@ -53,52 +67,53 @@ const LandingPage = () => {
                 >
                     Get Started
                 </Button>
+                <Container ref={featuresRef} maxWidth="md" sx={{ fontWeight: '400', lineHeight: 1.6, mt: 13, mb: 10, display: 'flex', flexDirection: 'column', justifyContent: 'center' }}>
+                    <Typography variant="h2" gutterBottom sx={{ textAlign: 'center', letterSpacing: '0.10em', mb: 5 }} >
+                        Features
+                    </Typography>
+                    <Stack container spacing={4}>
+                        <Grid item xs={12} md={4}>
+                            <Card sx={{ height: '100%', textAlign: 'center' }}>
+                                <CardContent>
+                                    <Typography variant="h5" gutterBottom>
+                                        Easy Expense Tracking
+                                    </Typography>
+                                    <Typography variant="body2">
+                                        Quickly add and categorize expenses, making it simple to keep track of your spending.
+                                    </Typography>
+                                </CardContent>
+                            </Card>
+                        </Grid>
+                        <Grid item xs={12} md={4}>
+                            <Card sx={{ height: '100%', textAlign: 'center' }}>
+                                <CardContent>
+                                    <Typography variant="h5" gutterBottom>
+                                        Budget Management
+                                    </Typography>
+                                    <Typography variant="body2">
+                                        Set and manage your budgets, ensuring you never overspend and stay on top of your finances.
+                                    </Typography>
+                                </CardContent>
+                            </Card>
+                        </Grid>
+                        <Grid item xs={12} md={4}>
+                            <Card sx={{ height: '100%', textAlign: 'center' }}>
+                                <CardContent>
+                                    <Typography variant="h5" gutterBottom>
+                                        Detailed Reports
+                                    </Typography>
+                                    <Typography variant="body2">
+                                        Generate insightful reports that help you analyze your spending patterns and make informed decisions.
+                                    </Typography>
+                                </CardContent>
+                            </Card>
+                        </Grid>
+                    </Stack>
+                </Container>
             </Container>
 
             {/* Features Section */}
-            <Container ref={featuresRef} maxWidth="md" sx={{ fontWeight: '400', lineHeight: 1.6, mt: 13, mb: 10, display: 'flex', flexDirection: 'column', justifyContent: 'center' }}>
-                <Typography variant="h2" gutterBottom sx={{ textAlign: 'center', letterSpacing: '0.10em', mb: 5 }} >
-                    Features
-                </Typography>
-                <Stack container spacing={4}>
-                    <Grid item xs={12} md={4}>
-                        <Card sx={{ height: '100%', textAlign: 'center' }}>
-                            <CardContent>
-                                <Typography variant="h5" gutterBottom>
-                                    Easy Expense Tracking
-                                </Typography>
-                                <Typography variant="body2">
-                                    Quickly add and categorize expenses, making it simple to keep track of your spending.
-                                </Typography>
-                            </CardContent>
-                        </Card>
-                    </Grid>
-                    <Grid item xs={12} md={4}>
-                        <Card sx={{ height: '100%', textAlign: 'center' }}>
-                            <CardContent>
-                                <Typography variant="h5" gutterBottom>
-                                    Budget Management
-                                </Typography>
-                                <Typography variant="body2">
-                                    Set and manage your budgets, ensuring you never overspend and stay on top of your finances.
-                                </Typography>
-                            </CardContent>
-                        </Card>
-                    </Grid>
-                    <Grid item xs={12} md={4}>
-                        <Card sx={{ height: '100%', textAlign: 'center' }}>
-                            <CardContent>
-                                <Typography variant="h5" gutterBottom>
-                                    Detailed Reports
-                                </Typography>
-                                <Typography variant="body2">
-                                    Generate insightful reports that help you analyze your spending patterns and make informed decisions.
-                                </Typography>
-                            </CardContent>
-                        </Card>
-                    </Grid>
-                </Stack>
-            </Container>
+            
             {/* Footer Section */}
             <Box sx={{ backgroundColor: '#f4f6f7', py: 3, mt: 'auto' }}>
                 <Container maxWidth="md">
