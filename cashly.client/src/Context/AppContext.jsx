c7a94ab3624fb897751ac7ed7d318008daadb175
eddyiiz <eddyxsayf@gmail.com>
2024-08-12 21:07:52 +0200

    spelling check

--- cashly.client/src/Context/AppContext.jsx
+++ cashly.client/src/Context/AppContext.jsx
@@ -4,19 +4,21 @@ import React, { useState, useContext, createContext } from 'react';
 const AppContext = createContext();
 
 //custom hook
-export const useAppContext = useContext(AppContext);
+export const useAppContext = () => useContext(AppContext);
 
 //provider component
 
-export const AppProvider = ({ childern }) => {
+const AppProvider = ({ children }) => {
     //state vars
     const [user, setUser] = useState(null);
 
     const [isAuthenticated, setIsAuthenticated] = useState(false); //login state
-
+        
     return (
         <AppContext.Provider value={{ user, setUser, isAuthenticated, setIsAuthenticated }} >
-            {childern}
+            {children}
         </AppContext.Provider>
     )
 }
+
+export default AppProvider;
