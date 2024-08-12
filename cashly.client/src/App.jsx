d3411e526a28889de8d20487ce42c109e20f61cb
eddyiiz <eddyxsayf@gmail.com>
2024-08-12 19:54:14 +0200

    wrap app with the app provider

--- cashly.client/src/App.jsx
+++ cashly.client/src/App.jsx
@@ -4,16 +4,19 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import theme from './theme';
 import LandingPage from './pages/LandingPage';
 import Dashboard from './Pages/Dashboard';
+import AppProvider from './Context/AppContext'
 
 function App() {
     return (
         <ThemeProvider theme={theme}>
-            <Router>
-                <Routes>
-                    <Route path="/" element={<LandingPage />} />
-                    <Route path="/dashboard" element={<Dashboard />} />
-                </Routes>
-            </Router>
+            <AppProvider>
+                <Router>
+                    <Routes>
+                        <Route path="/" element={<LandingPage />} />
+                        <Route path="/dashboard" element={<Dashboard />} />
+                    </Routes>
+                </Router>
+            </AppProvider>
         </ThemeProvider>
     );
 }
