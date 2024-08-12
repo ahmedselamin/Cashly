b235ad757f52d232bb9ab9ef960600827aa494f1
eddyiiz <eddyxsayf@gmail.com>
2024-08-12 20:40:07 +0200

    Protected route component

---
+++ cashly.client/src/Components/ProtectedRoute.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Navigate } from 'react-router-dom';
+import { useAppContext } from '../context/AppContext';
+
+const ProtectedRoute = ({ children }) => {
+    const { isAuthenticated } = useAppContext();
+
+    return isAuthenticated ? children : <Navigate to="/" />;
+};
+
+export default ProtectedRoute;
