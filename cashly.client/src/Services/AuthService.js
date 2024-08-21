441c0e0433a98aabb2d67000a5eeb20a5bddcae1
eddyiiz <eddyxsayf@gmail.com>
2024-08-21 10:02:45 +0200

    fixing response

--- cashly.client/src/Services/AuthService.js
+++ cashly.client/src/Services/AuthService.js
@@ -5,14 +5,21 @@ const API_URL = "https://localhost:7205/api/Auth";
 //user register
 export const register = async () => {
     const reponse = await axios.post(`${API_URL}/register`, { username, password });
-    return response.data;
+
+    if (response.data.Success) {
+        return response.data.Data
+    } else {
+        throw new Error(response.data.Message);
+    }
 }
 
 // user login
 export const login = async (username, password) => {
     const response = await axios.post(`${API_URL}/login`, { username, password });
-    if (response.data.token) {
-        localStorage.setItem('user', JSON.stringify(response.data));
+    if (response.data.Success) {
+        localStorage.setItem('user', JSON.stringify(response.data.data));  // Store user data
+    } else {
+        throw new Error(response.data.Message);
     }
     return response.data;
 };
